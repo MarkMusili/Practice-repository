64348e2abe2a020bc7fd691508a52d688b3a2e20
MarkMusili <musiliyrn@gmail.com>
2023-05-15 10:40:30 +0300

    Ready to start the printf project

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *Header files
+*/
+#include <stdio.h>
+
+#endif
--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h.>
 int main(void)
 {
-    printf("Hello this  is my first try using VS Code yaay it worked");
+    printf("Hello this  is my first try using VS Code.\n");
+    printf("Now i'm ready to start the printf project.");
     return (0);
 }
